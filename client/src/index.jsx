7589c97ee6f40b1f8859d49a4fb2f1dc04a7579c
Julia McNeill <juliammcneill@gmail.com>
2020-07-30 12:46:47 -0400

    Set up SASS and adjust dependencies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import "./style.scss";
 
 class App extends React.Component {
   constructor() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: DIST_DIR,
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015'],
+          presets: ["@babel/preset-env", "@babel/preset-react"],
         },
+      },
+      {
+        test: /\.scss$/i,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
