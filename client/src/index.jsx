84e7841e26daee71f6f7e30fa0c675661cb58164
Julia McNeill <juliammcneill@gmail.com>
2020-07-31 10:44:53 -0400

    Add fade-in transitions to all views

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,16 +8,44 @@ import Results from "./components/results.jsx";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { view: "start", results: [] };
+    this.state = {
+      view: "start",
+      results: [],
+      startTransition: "100%",
+      questionsTransition: "0%",
+      resultsTransition: "0%",
+    };
 
     this.changeView = this.changeView.bind(this);
     this.updateResults = this.updateResults.bind(this);
   }
 
   changeView(option) {
-    this.setState({
-      view: option,
-    });
+    setTimeout(() => {
+      this.setState({
+        view: option,
+      });
+    }, 250);
+
+    if (option === "start") {
+      this.setState({
+        startTransition: "100%",
+        questionsTransition: "0%",
+        resultsTransition: "0%",
+      });
+    } else if (option === "questions") {
+      this.setState({
+        startTransition: "0%",
+        questionsTransition: "100%",
+        resultsTransition: "0%",
+      });
+    } else if (option === "results") {
+      this.setState({
+        startTransition: "0%",
+        questionsTransition: "0%",
+        resultsTransition: "100%",
+      });
+    }
   }
 
   updateResults(data) {
@@ -29,20 +57,50 @@ class App extends React.Component {
   renderView() {
     if (this.state.view === "start") {
       return (
-        <button onClick={() => this.changeView("questions")}>
-          Create New Tour
-        </button>
+        <div
+          style={{
+            opacity: this.state.startTransition,
+            "-webkit-transition": "opacity .25s",
+            "-moz-transition": "opacity .25s",
+            "-o-transition": "opacity .25s",
+            transition: "opacity .25s",
+          }}
+        >
+          <button onClick={() => this.changeView("questions")}>
+            Create New Tour
+          </button>
+        </div>
       );
     } else if (this.state.view === "questions") {
       return (
-        <Questions
-          updateResults={this.updateResults}
-          changeView={this.changeView}
-        />
+        <div
+          style={{
+            opacity: this.state.questionsTransition,
+            "-webkit-transition": "opacity .5s",
+            "-moz-transition": "opacity .5s",
+            "-o-transition": "opacity .5s",
+            transition: "opacity .5s",
+          }}
+        >
+          <Questions
+            updateResults={this.updateResults}
+            changeView={this.changeView}
+          />
+        </div>
       );
     } else if (this.state.view === "results") {
       return (
-        <Results results={this.state.results} changeView={this.changeView} />
+        <div
+          style={{
+            opacity: this.state.resultsTransition,
+            "-webkit-transition": "opacity .5s",
+            "-moz-transition": "opacity .5s",
+            "-o-transition": "opacity .5s",
+            transition: "opacity .5s",
+          }}
+        >
+          <Results results={this.state.results} changeView={this.changeView} />
+        </div>
       );
     }
   }
