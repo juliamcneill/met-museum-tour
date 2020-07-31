7f5150c5fce54d7edfc4117f03744d99faae5a53
Julia McNeill <juliammcneill@gmail.com>
2020-07-31 15:19:02 -0400

    Add nodemon dependency and webpack script

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.set("port", 3500);
+app.set("port", 3000);
 
 app.use(bodyParser.json());
 app.use("/", require("./calls"));
