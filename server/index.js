ddb845b989a1ced58fa7c42f89557962b027deda
Julia McNeill <juliammcneill@gmail.com>
2020-09-08 22:13:18 -0400

    Apply ENV variable for port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.set("port", 3000);
+const port = process.env.PORT || 3000;
+
+app.set("port", port);
 
 app.use(bodyParser.json());
 app.use("/", require("./calls"));
