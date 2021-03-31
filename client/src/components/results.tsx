b752e47fa779e385f9a12ea9e4321b664e3effd7
Julia McNeill <juliammcneill@gmail.com>
2021-03-30 23:33:38 -0400

    Major refactor from classes to functional components

--- client/src/components/results.tsx
+++ client/src/components/results.tsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react';
+import met from '../../dist/assets/met.jpg';
+import rightArrow from '../../dist/assets/rightArrow.jpg';
+import leftArrow from '../../dist/assets/leftArrow.jpg';
 
-import met from "../../dist/assets/met.jpg";
-import rightArrow from "../../dist/assets/rightArrow.jpg";
-import leftArrow from "../../dist/assets/leftArrow.jpg";
-
-interface MyProps {
+interface Props {
   results: any;
   changeView: Function;
 }
-
 interface Object {
   artistDisplayName?: string;
   title?: string;
@@ -17,67 +15,52 @@ interface Object {
   primaryImage?: string;
 }
 
-class Results extends React.Component<MyProps> {
-  constructor(props: { results: any; changeView: Function }) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.props.changeView("start");
-  }
-
-  render() {
-    return (
-      <div>
-        <img
-          src={met}
-          alt="Line drawing of the Metropolitan Museum of Art"
-        ></img>
-        {Object.keys(this.props.results).length === 0 ? (
-          <div id="errorMessage">Not enough results! Try again.</div>
-        ) : (
-          Object.keys(this.props.results).map((key, index) => (
-            <div>
-              <img
-                className={index % 2 === 0 ? "left-arrow" : "right-arrow"}
-                src={index % 2 === 0 ? leftArrow : rightArrow}
-                alt="Decorative arrow"
-              ></img>
-              <div
-                className={
-                  index % 2 === 0
-                    ? "right-department-name-container"
-                    : "left-department-name-container"
-                }
-              >
-                <div className="department-name">{key}</div>
-              </div>
-              {this.props.results[key].map((object: Object) => (
-                <div className="object-info">
-                  <div className="object-name">{object.artistDisplayName}</div>
-                  <div className="object-title">{object.title}</div>
-                  <div className="object-medium">{object.medium}</div>
-                  <div className="object-date">{object.objectDate}</div>
-                  <img
-                    className="object-image"
-                    src={object.primaryImage}
-                    alt="Reproduction of artwork"
-                  />
-                </div>
-              ))}
-              {console.log(index)}
+const Results: React.FC<Props> = ({ results, changeView }) => {
+  return (
+    <div>
+      <img src={met} alt="Line drawing of the Metropolitan Museum of Art"></img>
+      {Object.keys(results).length === 0 ? (
+        <div id="errorMessage">Not enough results! Try again.</div>
+      ) : (
+        Object.keys(results).map((key, index) => (
+          <div>
+            <img
+              className={index % 2 === 0 ? 'left-arrow' : 'right-arrow'}
+              src={index % 2 === 0 ? leftArrow : rightArrow}
+              alt="Decorative arrow"
+            ></img>
+            <div
+              className={
+                index % 2 === 0
+                  ? 'right-department-name-container'
+                  : 'left-department-name-container'
+              }
+            >
+              <div className="department-name">{key}</div>
             </div>
-          ))
-        )}
+            {results[key].map((object: Object) => (
+              <div className="object-info">
+                <div className="object-name">{object.artistDisplayName}</div>
+                <div className="object-title">{object.title}</div>
+                <div className="object-medium">{object.medium}</div>
+                <div className="object-date">{object.objectDate}</div>
+                <img
+                  className="object-image"
+                  src={object.primaryImage}
+                  alt="Reproduction of artwork"
+                />
+              </div>
+            ))}
+            {console.log(index)}
+          </div>
+        ))
+      )}
 
-        <button type="submit" onClick={this.handleClick}>
-          Generate Another Tour
-        </button>
-      </div>
-    );
-  }
-}
+      <button type="submit" onClick={() => changeView('start')}>
+        Generate Another Tour
+      </button>
+    </div>
+  );
+};
 
 export default Results;
