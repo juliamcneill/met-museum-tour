5557061571a44cb3987510eb7287c8d150aeeeba
Julia McNeill <juliammcneill@gmail.com>
2023-10-17 14:40:46 -0400

    MET-8: Add background color and bounding boxes with white background

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,6 @@
 import leftArrow from "../assets/leftArrow.jpg";
-import met from "../assets/met.jpg";
 import rightArrow from "../assets/rightArrow.jpg";
+import MetImage from "./MetImage";
 import { UIButton } from "./UIButton";
 import React, { useCallback } from "react";
 
@@ -23,46 +23,50 @@ const Results: React.FC<Props> = ({ results, changeView }) => {
 
     return (
         <div>
-            <img
-                className="mx-auto my-4 block w-4/5"
-                src={met}
-                alt="Line drawing of the Metropolitan Museum of Art"
-            ></img>
-            {Object.keys(results).length === 0 ? (
-                <div id="errorMessage">Not enough results! Try again.</div>
-            ) : (
-                Object.keys(results).map((key, index) => (
-                    <div key={key}>
-                        <img
-                            className={`mb-[20px] mt-[35px] block w-1/3 ${
-                                index % 2 === 0 ? "float-right" : "float-left"
-                            }`}
-                            src={index % 2 === 0 ? leftArrow : rightArrow}
-                            alt="Decorative arrow"
-                        ></img>
-                        <div
-                            className={`relative mb-[5px] mt-[20px] block h-[150px] w-3/5 ${
-                                index % 2 === 0 ? "float-right" : "float-left"
-                            }`}
-                        >
-                            <div className="top-/4 absolute top-1/2 m-auto text-2xl decoration-solid">{key}</div>
-                        </div>
-                        {results[key].map((object: Object) => (
-                            <div className="clear-both" key={key + object.title}>
-                                <div>{object.artistDisplayName}</div>
-                                <div className="font-light">{object.title}</div>
-                                <div className="text-sm">{object.medium}</div>
-                                <div className="text-sm">{object.objectDate}</div>
+            <MetImage />
+            <div className="mt-8 flex flex-col gap-8 rounded border border-primary bg-white p-8">
+                {Object.keys(results).length === 0 ? (
+                    <div className="flex justify-center">Not enough results! Try again.</div>
+                ) : (
+                    Object.keys(results).map((key, index) => (
+                        <div className="flex flex-col" key={key}>
+                            <div>
                                 <img
-                                    className=" mx-auto my-4 mb-6 block w-3/5"
-                                    src={object.primaryImage}
-                                    alt="Reproduction of artwork"
-                                />
+                                    className={`mb-[20px] mt-[35px] block w-1/3 ${
+                                        index % 2 === 0 ? "float-right" : "float-left"
+                                    }`}
+                                    src={index % 2 === 0 ? leftArrow : rightArrow}
+                                    alt="Decorative arrow"
+                                ></img>
+                                <div
+                                    className={`relative mb-[5px] mt-[20px] block h-[150px] w-3/5 ${
+                                        index % 2 === 0 ? "float-right" : "float-left"
+                                    }`}
+                                >
+                                    <div className="top-/4 absolute top-1/2 m-auto text-2xl decoration-solid">
+                                        {key}
+                                    </div>
+                                </div>
                             </div>
-                        ))}
-                    </div>
-                ))
-            )}
+                            {results[key].map((object: Object) => (
+                                <div className="align-center flex" key={key + object.title}>
+                                    <div className="w-2/5 p-8">
+                                        <div>{object.artistDisplayName}</div>
+                                        <div className="font-light">{object.title}</div>
+                                        <div className="text-sm">{object.medium}</div>
+                                        <div className="text-sm">{object.objectDate}</div>
+                                    </div>
+                                    <img
+                                        className="mx-auto my-4 mb-6 block w-1/2"
+                                        src={object.primaryImage}
+                                        alt="Reproduction of artwork"
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    ))
+                )}
+            </div>
             <UIButton className="mx-auto my-4 mt-8 block w-4/5" onClick={changeViewToStart}>
                 Generate Another Tour
             </UIButton>
